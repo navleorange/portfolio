459e6b19ca515ea7840a78d2566f96e69f4a0431
navleorange <navleorange.0718@gmail.com>
2024-04-05 08:08:15 +0900

    add: hamburger menu base

--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -15,11 +15,37 @@ body{
     position: fixed;
     top: 0%;
 
+    // 子要素を右寄せに
+    display: flex;
+    justify-content: flex-end;
+
     // 子要素を中央に
     align-items: center;
 
+    // ハンバーガーメニューを表示する時のcss
+    &.hamburger_menu_active{
+        // ボタンとメニュー要素を縦一列にする
+        flex-flow: column;
+
+        // メニューボタンを右寄せ
+        align-items: flex-end;
+
+        // メニューを右に出す
+        width: 35%;
+        right: 0;
+
+        opacity: 0.5;
+
+        z-index: 100;
+    }
+
     &:hover{
         opacity: 1;
+
+        &.hamburger_menu_active{
+            opacity: 0.9;
+        }
+
     }
 
     .hamburger_menu_button{
@@ -27,8 +53,30 @@ body{
 
         @include mq(sp){
             // スマホのみ表示
-            display: block;
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: end;
+            width: 50px;
+            height: 50px;
+            padding: 5px;
+            cursor: pointer;
         }
+
+        // ハンバーガーメニューの線
+        span{
+            position: absolute;
+            display: block;
+            width: 30px;
+            height: 5px;
+            background: #019ac6;
+        }
+        @for $spanIndex from 1 through 3{
+            span:nth-of-type(#{$spanIndex}){
+                top: 10px + 8px*$spanIndex;
+            }
+        }
+
     }
 
     // Header Menu
@@ -40,7 +88,33 @@ body{
         display: flex;
         justify-content: flex-end;
 
+        @include mq(sp){
+            // スマホは最初は表示しない
+            display: none;
+
+            // このクラスが付与されたら見えるようにする
+            &.appear{
+                // navと同じにする
+                width: 100%;
+
+                // 表示と縦指定
+                display: flex;
+                flex-flow: column;
+            }
+
+        }
+
         li{
+
+            .internal_link{
+                // ハンバーガーメニューを表示する時のcss
+                &.hamburger_menu_active{
+                    // 子要素を中央揃え
+                    margin-right: auto;
+                    margin-left: auto;
+                    margin-bottom: 1.5rem;
+                }
+            }
 
             .link_text{
                 // 上下, 左右
@@ -78,15 +152,21 @@ body{
 
 </style>
 
-<script scoped>
+<script lang="ts" scoped>
     export default {
         data() {
             return {
+                isHamburgerMenuOpen: false,
                 links: [
                     { label:"Top", isInternalLink: true , path: '/profile'},
                     { label:"制作物", isInternalLink: true , path: '/works'},
                     { label:"研究内容", isInternalLink: true , path: '/search'},
                 ]
+            }
+        },
+        methods:{
+            hamburgerMenuClickEvent(){
+                this.isHamburgerMenuOpen = !this.isHamburgerMenuOpen;
             }
         }
     }
@@ -94,16 +174,17 @@ body{
 
 <template>
     <header>
-        <nav class="header-nav">
+        <nav v-bind:class="{'hamburger_menu_active': isHamburgerMenuOpen}" class="header-nav">
 
             <!-- スマホ用のハンバーガーメニュー -->
-            <div class="hamburger_menu_button">
-                <input type="checkbox">
+            <div @click="hamburgerMenuClickEvent" class="hamburger_menu_button">
+                <!-- ハンバーガーメニューの線 -->
+                <span></span><span></span><span></span>
             </div>
 
-            <ul>
+            <ul v-bind:class="{'appear': isHamburgerMenuOpen}">
                 <li v-for="(link, i) in links" :key="i">
-                    <div v-if="link.isInternalLink" class="internal_link">
+                    <div v-if="link.isInternalLink" v-bind:class="{'hamburger_menu_active': isHamburgerMenuOpen}" class="internal_link">
                         <NuxtLink :to="link.path" class="link_text">{{ link.label }}</NuxtLink>
                     </div>
                     <div v-else class="external_link">
