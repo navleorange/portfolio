05574523ad844265d7ab747118bb98f3a382cd66
navleorange <navleorange.0718@gmail.com>
2024-04-25 17:58:31 +0900

    add: add mark down css

---
+++ components/content/MarkDownBase.vue
@@ -0,0 +1,27 @@
+<style lang="scss" scoped>
+
+.markdown_content{
+    width: 80%;
+    background-color: whitesmoke;
+
+    // ブロックの中央揃え
+    margin-left: auto;
+    margin-right: auto;
+
+    // 背景と文字の余白 上下 | 左右
+    padding: 1% 3%;
+
+    :deep(h2), :deep(h3), :deep(h4){
+        // border-left: 0.125rem solid gray;
+        border-bottom: 0.25rem solid gray;
+    }
+
+}
+
+</style>
+
+<template>
+    <div class="markdown_content">
+        <slot />
+    </div>
+</template>
--- pages/search.vue
+++ pages/search.vue
@@ -4,13 +4,19 @@
     width: 100vw;
     height: 100vh;
     margin-top: 5%;
+
+    @include mq(sp){
+        margin-top: 20%;
+        font-size: 10px;
+    }
+
 }
 
 </style>
 
 <template>
     <div id="search_container">
-        <ContentDoc path="/search/test" v-slot="{ doc }">
+        <ContentDoc path="/research/research" v-slot="{ doc }">
             <ContentRenderer :value="doc" />
         </ContentDoc>
     </div>
