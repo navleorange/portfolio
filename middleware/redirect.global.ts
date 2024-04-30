6431bc087cda5846daaff0ee5b58a636cac47eaf
navleorange <navleorange.0718@gmail.com>
2024-04-30 21:52:07 +0900

    add: part time job info

--- middleware/redirect.global.ts
+++ middleware/redirect.global.ts
@@ -2,7 +2,7 @@ import { navigateTo } from "nuxt/app"
 
 export default defineNuxtRouteMiddleware((to, from) => {
   
-  if(to.path !== '/profile' && to.path !== '/research' && to.path !== "/works"){
+  if(to.path !== '/profile' && to.path !== '/research' && to.path !== "/works" && to.path !== "/job"){
     return navigateTo('/profile')
   }
 })
---
+++ pages/job.vue
@@ -0,0 +1,23 @@
+<style lang="scss" scoped>
+
+#job_container{
+    width: 100vw;
+    height: 100vh;
+    margin-top: 5%;
+
+    @include mq(sp){
+        margin-top: 20%;
+        font-size: 10px;
+    }
+
+}
+
+</style>
+
+<template>
+    <div id="job_container">
+        <ContentDoc path="/job/job" v-slot="{ doc }">
+            <ContentRenderer :value="doc" />
+        </ContentDoc>
+    </div>
+</template>
