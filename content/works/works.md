ae606d718602980d61d94d5c55091ed40e7c1647
navleorange <navleorange.0718@gmail.com>
2024-05-01 12:36:13 +0900

    add: team works explain

---
+++ content/works/works.md
@@ -0,0 +1,17 @@
+::MarkDownBase
+---
+title: 制作物
+description: '制作物を紹介をするところです'
+---
+
+# 制作物
+
+## 個人開発
+
+## チーム開発
+
+### szpp-judge
+ランキングシステムを担当しました。(詳細は随時記載します)<br>
+[GitHub](https://github.com/szpp-dev-team/szpp-judge)
+
+::
--- pages/works.vue
+++ pages/works.vue
@@ -1,3 +1,23 @@
+<style lang="scss" scoped>
+
+#works_container{
+    width: 100vw;
+    height: 100vh;
+    margin-top: 5%;
+
+    @include mq(sp){
+        margin-top: 20%;
+        font-size: 10px;
+    }
+
+}
+
+</style>
+
 <template>
-    <UnderConstruction />
+    <div id="works_container">
+        <ContentDoc path="/works/works" v-slot="{ doc }">
+            <ContentRenderer :value="doc" />
+        </ContentDoc>
+    </div>
 </template>
