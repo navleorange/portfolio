71642c69cf76bd9f082f5fbc71b8d47d85e35d01
navleorange <navleorange.0718@gmail.com>
2024-05-06 18:47:49 +0900

    fix: divide works group

--- pages/works.vue
+++ pages/works.vue
@@ -37,11 +37,12 @@
         text-align: center;
     }
 
-    .private_works, .team_works{
+    .private_work, .team_work{
         width: 100%;
         max-width: 600px;
         height: 250px;
         border: 1px solid gray;
+        background-color: red;
 
         @include mq(full){
             min-width: 600px;
@@ -100,49 +101,99 @@
 
 <script setup lang="ts">
 
+interface displayData{
+    title: string;
+    lang: string;
+    GitHubLink: string;
+    description: string;
+    MarkDownPath: string;
+}
+
 // search /content/works contents 
-const { data:content} = await useAsyncData("works", () =>
+const { data:works } = await useAsyncData("works", () =>
 queryContent("works").find())
+
+
+// divide
+let private_works: displayData[] = [];
+let research_works: displayData[] = [];
+let team_works: displayData[] = [];
+
+// works内で値が入っている場所が_rawValue
+works._rawValue.forEach((work) => {
+
+    let addWork: displayData = {
+        title: work.title, 
+        lang: work.lang,
+        GitHubLink: work.GitHub,
+        description: work.description,
+        MarkDownPath: work._path
+    }
+
+    if(addWork.MarkDownPath.includes('private')){
+        private_works.push(addWork);
+    }
+    else if(addWork.MarkDownPath.includes("research")){
+        research_works.push(addWork);
+    }
+    else if(addWork.MarkDownPath.includes("team")){
+        team_works.push(addWork);
+    }
+
+})
 
 </script>
 
 <template>
     <div id="works_container">
-
-        <div class="private_works_container">
+        <div v-if="private_works.length !== 0" class="private_works_container">
             <h1>個人開発</h1>
-            <div v-for="private_content in content" :key="private_content._path">
-                <div v-if="private_content._path.includes('private')">
-                    <div class="private_works">
-                        <h2>{{private_content.title}}</h2>
-                        <table>
-                            <tbody>
-                                <tr><th>言語</th><td>{{private_content.lang}}</td></tr>
-                                <tr><th>概要</th><td>{{private_content.description}}</td></tr>
-                                <tr><th>GitHub</th><td><a v-bind:href="private_content.GitHub">リポジトリを見る</a> </td></tr>
-                                <tr><th>説明</th><td><nuxt-link :to="private_content._path">説明を確認する</nuxt-link></td></tr>
-                            </tbody>
-                        </table>
-                    </div>
+            <div v-for="private_work in private_works" :key="private_work.MarkDownPath">
+                <div class="private_work">
+                    <!-- 本当ならここ以下は共通化したい -->
+                    <h2>{{private_work.title}}</h2>
+                    <table>
+                        <tbody>
+                            <tr><th>言語</th><td>{{private_work.lang}}</td></tr>
+                            <tr><th>概要</th><td>{{private_work.description}}</td></tr>
+                            <tr><th>GitHub</th><td><a v-bind:href="private_work.GitHubLink">リポジトリを見る</a> </td></tr>
+                            <tr><th>説明</th><td><nuxt-link :to="private_work.MarkDownPath">説明を確認する</nuxt-link></td></tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
 
-        <div class="team_works_container">
+        <div v-if="research_works.length !== 0" class="research_works_container">
+            <h2>研究関連の開発物</h2>
+            <div v-for="research_work in research_works" :key="research_work.MarkDownPath">
+                <div class="private_work">
+                    <h2>{{research_work.title}}</h2>
+                    <table>
+                        <tbody>
+                            <tr><th>言語</th><td>{{research_work.lang}}</td></tr>
+                            <tr><th>概要</th><td>{{research_work.description}}</td></tr>
+                            <tr><th>GitHub</th><td><a v-bind:href="research_work.GitHubLink">リポジトリを見る</a> </td></tr>
+                            <tr><th>説明</th><td><nuxt-link :to="research_work.MarkDownPath">説明を確認する</nuxt-link></td></tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div v-if="team_works.length !== 0" class="team_works_container">
             <h1>チーム開発</h1>
-            <div v-for="team_content in content" :key="team_content._path">
-                <div v-if="team_content._path.includes('team')">
-                    <div class="team_works">
-                        <h2>{{team_content.title}}</h2>
-                        <table>
-                            <tbody>
-                                <tr><th>言語</th><td>{{team_content.lang}}</td></tr>
-                                <tr><th>概要</th><td>{{team_content.description}}</td></tr>
-                                <tr><th>GitHub</th><td><a v-bind:href="team_content.GitHub">リポジトリを見る</a> </td></tr>
-                                <tr><th>説明</th><td><nuxt-link :to="team_content._path">説明を確認する</nuxt-link></td></tr>
-                            </tbody>
-                        </table>
-                    </div>
+            <div v-for="team_work in team_works" :key="team_work.MarkDownPath">
+                <div class="team_work">
+                    <h2>{{team_work.title}}</h2>
+                    <table>
+                        <tbody>
+                            <tr><th>言語</th><td>{{team_work.lang}}</td></tr>
+                            <tr><th>概要</th><td>{{team_work.description}}</td></tr>
+                            <tr><th>GitHub</th><td><a v-bind:href="team_work.GitHubLink">リポジトリを見る</a> </td></tr>
+                            <tr><th>説明</th><td><nuxt-link :to="team_work.MarkDownPath">説明を確認する</nuxt-link></td></tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
